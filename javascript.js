8ae540db130d78d9e09aab647404380fa7f98f95
Seif Boudokhane <113269052+Seifboudokhane@users.noreply.github.com>
2023-09-15 12:53:54 +0100

    Make function that plays a round & returns winner

--- javascript.js
+++ javascript.js
@@ -12,4 +12,20 @@ function getComputerChoice(){
             break;
     }
 }
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.trim();
+    playerSelection = playerSelection.toUpperCase();
+
+    if (playerSelection == computerSelection){
+        return "It's A Draw!";
+    }else if((playerSelection == "Rock" && computerSelection == "SCISSORS")||(playerSelection == "PAPER" && computerSelection == "ROCK") || (playerSelection == "SCISSORS" && computerSelection == "PAPER")){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+const playerSelection = "   paPer ";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
