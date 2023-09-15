8d316cad3f7bbf5a86465ed055e786d33b2d215f
Seif Boudokhane <113269052+Seifboudokhane@users.noreply.github.com>
2023-09-15 13:27:16 +0100

    Write a function that plays a game of 5 rounds

--- javascript.js
+++ javascript.js
@@ -18,14 +18,43 @@ function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toUpperCase();
 
     if (playerSelection == computerSelection){
-        return "It's A Draw!";
+        console.log("It's A Draw!");
+        return 0;
     }else if((playerSelection == "Rock" && computerSelection == "SCISSORS")||(playerSelection == "PAPER" && computerSelection == "ROCK") || (playerSelection == "SCISSORS" && computerSelection == "PAPER")){
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        return 1;
     }else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return -1;
     }
 }
 
-const playerSelection = "   paPer ";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+function game(){
+    let playerScore=0;
+    let computerScore=0;
+    let roundResult;
+    for (let i=0;i<5;i++){
+        let playerSelection=prompt("choose Rock, Paper or scissors");
+        let computerSelection=getComputerChoice();
+        roundResult = playRound(playerSelection,computerSelection);
+        switch(roundResult){
+            case -1:
+                computerScore++;
+                break;
+            case 1:
+                playerScore++;
+                break;
+            case 0:
+                break;
+        }
+    }
+    if(playerScore==computerScore){
+        return "IT'S A DRAW!";
+    }else if(playerScore>computerScore){
+        return "YOU WIN!";
+    }else {
+        return "YOU LOSE!";
+    }
+}
+
+console.log(game());
