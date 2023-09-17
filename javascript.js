0591ec0e48ea482ebbc802dc1e83f7056f0608c7
Seif Boudokhane <113269052+Seifboudokhane@users.noreply.github.com>
2023-09-17 16:16:32 +0100

    Add a button that restarts the game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,35 @@
+let playerSelection;
+let playerScore=0;
+let computerScore=0;
+
+function playRound(playerSelection){
+    if(computerScore==5||playerScore==5){
+        return;
+    }
+
+    let computerSelection = getComputerChoice();
+
+    console.log("you chose "+playerSelection);
+
+    if (playerSelection == computerSelection){
+        document.getElementById("roundResult").textContent=(`Draw! you both chose ${computerSelection}`);
+    }else if((playerSelection == "rock" && computerSelection == "scissors")||(playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")){
+        document.getElementById("roundResult").textContent=(`You Win! ${playerSelection} beats ${computerSelection}`);
+        playerScore++;
+    }else {
+        document.getElementById("roundResult").textContent=(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        computerScore++;
+    }
+
+    document.getElementById("playerScore").textContent=playerScore;
+    document.getElementById("computerScore").textContent=computerScore;
+
+    if(computerScore==5||playerScore==5){
+        document.getElementById("gameOver").textContent=('GAME OVER! ');
+        playerScore>computerScore?document.getElementById("gameOver").textContent+=('YOU WON!'):document.getElementById("gameOver").textContent+=('YOU LOST!')
+        return;
+    }
+}
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     switch(randomNumber){
@@ -12,37 +44,19 @@ function getComputerChoice(){
             break;
     }
 }
-
-function playRound(playerSelection){
-    if(computerScore==5||playerScore==5){
-        return;
-    }
-    let computerSelection = getComputerChoice();
-    console.log("you chose "+playerSelection);
-    if (playerSelection == computerSelection){
-        document.getElementById("roundResult").textContent=(`Draw! you both chose ${computerSelection}`);
-    }else if((playerSelection == "rock" && computerSelection == "scissors")||(playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")){
-        document.getElementById("roundResult").textContent=(`You Win! ${playerSelection} beats ${computerSelection}`);
-        playerScore++;
-    }else {
-        document.getElementById("roundResult").textContent=(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        computerScore++;
-    }
+function restartGame(){
+    playerScore=0;
+    computerScore=0;
     document.getElementById("playerScore").textContent=playerScore;
     document.getElementById("computerScore").textContent=computerScore;
-    if(computerScore==5||playerScore==5){
-        document.getElementById("gameOver").textContent=('GAME OVER! ');
-        playerScore>computerScore?document.getElementById("gameOver").textContent+=('YOU WON!'):document.getElementById("gameOver").textContent+=('YOU LOST!')
-        return;
-    }
+    document.getElementById("gameOver").textContent="";
 }
-let playerSelection;
-let playerScore=0;
-let computerScore=0;
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
+const restart = document.querySelector(".restart");
 
 rock.addEventListener("click",()=>playRound("rock"));
 paper.addEventListener("click",()=>playRound("paper"));
 scissors.addEventListener("click",()=>playRound("scissors"));
+restart.addEventListener("click", ()=>restartGame());
